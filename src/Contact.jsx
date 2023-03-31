c6ae85a0cdfb7f67d29f83c17a2a2e37274adaf1
surya <suryaprakashr1025@gmail.com>
2023-03-31 09:55:18 +0530

    push 31.03.2023

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -12,27 +12,65 @@ function Contact() {
 
 
 
-        <div className='container-fluid col-12' style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+        // <div className='container-fluid' style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
 
-            <div style={{ padding: "150px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        //     <div class="col-lg-6 col-md-6 col-12" style={{ padding: "150px", display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
 
-                <h1 className='mb-5' style={{ color: "blue", fontWeight: 'bold' }}>Contact Us</h1>
-                <div>
-                    
-                    <a class="card-title mb-4" style={{ cursor: "pointer", color: "black",padding:"15px" }} href="mailto:suryaprakashr1025@gmail.com" target="_blank"><MdEmail style={{width:"50px",height:"50px"}}/></a>
-                    <a class="card-title mb-4" style={{ cursor: "pointer", color: "black",padding:"15px"  }} href="https://github.com/suryaprakashr1025/" target="_blank"><RxGithubLogo style={{width:"50px",height:"50px"}}/></a>
-                    <a class="card-title mb-4" style={{ cursor: "pointer", color: "black",padding:"15px"  }} href="https://www.linkedin.com/in/suryaprakashr1025/" target="_blank"><BsLinkedin style={{width:"50px",height:"50px"}}/></a>
-                    <h3 class="card-title mt-4" style={{fontWeight:"bold"}}><IoMdCall style={{width:"50px",height:"50px"}}/>9566953853</h3>
-               
+        //         <h3 className='mb-5' style={{ color: "blue", fontWeight: 'bold' }}>Contact Us</h3>
+        //         <div>
+
+        //             <a class="card-title " style={{ cursor: "pointer", color: "black",padding:"15px" }} href="mailto:suryaprakashr1025@gmail.com" target="_blank"><MdEmail style={{width:"50px",height:"50px"}}/></a>
+        //             <a class="card-title " style={{ cursor: "pointer", color: "black",padding:"15px"  }} href="https://github.com/suryaprakashr1025/" target="_blank"><RxGithubLogo style={{width:"50px",height:"50px"}}/></a>
+        //             <a class="card-title " style={{ cursor: "pointer", color: "black",padding:"15px"  }} href="https://www.linkedin.com/in/suryaprakashr1025/" target="_blank"><BsLinkedin style={{width:"50px",height:"50px"}}/></a>
+        //             <h3 class="card-title mt-4" style={{fontWeight:"bold"}}><IoMdCall style={{width:"35px",height:"20px"}}/>9566953853</h3>
+
+        //         </div>
+
+
+        //     </div>
+
+        //     <div class="col-lg-6 col-md-6 col-12" style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        //         <img src={contact} class="img-fluid" style={{ width: "80%" }} />
+        //     </div>
+        // </div>
+
+        <div className='container-fluid col-12'>
+            <section id="home">
+                <div class="row">
+                    <div class="col-lg-6 col-md-6 col-12 order-1 pt-5 con" style={{ display: "flex", flexDirection: "column", justifyContent: "center", padding: "0px 150px" }}>
+                        <h3 className='mx-auto mb-5' style={{ fontWeight: "1000", color: "blue" }}>Contact Us</h3>
+                        <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center" }}>
+                       
+                       
+                       <div style={{ display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                       <a class="card-title mail" style={{ cursor: "pointer", color: "black"  }} href="mailto:suryaprakashr1025@gmail.com" target="_blank"><MdEmail style={{ width: "50px", height: "50px" }} /></a>
+                       <label className='e'>Send Mail</label>
+                        
+                       </div>
+                        <div style={{ display: "flex", flexDirection: "column", justifyContent: "center"}}>
+                        <a class="card-title git" style={{ cursor: "pointer", color: "black" }} href="https://github.com/suryaprakashr1025/" target="_blank"><RxGithubLogo style={{ width: "50px", height: "50px" }} /></a>
+                        <label className='g text-center'>View Code</label>
+                     
+                        </div>
+                       <div style={{ display: "flex", flexDirection: "column", justifyContent: "center" }}>
+                       <a class="card-title linked" style={{ cursor: "pointer", color: "black" }} href="https://www.linkedin.com/in/suryaprakashr1025/" target="_blank"><BsLinkedin style={{ width: "50px", height: "50px" }} /></a>
+                       <label className='l'>View LinkedIn Profile</label>
+                       </div>
+                      
+                       </div>
+                      
+                        <h5 class="card-title mt-4 mx-auto" style={{ fontWeight: "bold" }}><IoMdCall  />9566953853</h5>
+
+
+                    </div>
+                    <div class="col-lg-6 col-md-6 col-12 py-lg-0 py-3 order-sm-2 " style={{ display: "flex", flexDirection: "row", justifyContent: "flexend", alignItems: "flexend" }}>
+                        <img src={contact} class="img-fluid contactimg" style={{ width: "80%" }} />
+                    </div>
                 </div>
-
-
-            </div>
-
-            <div class="col-lg-6 col-md-6 col-12 py-lg-0 py-3 order-sm-2" style={{ display: "flex", flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-                <img src={contact} class="img-fluid" style={{ width: "80%" }} />
-            </div>
+            </section>
         </div>
+
+
     )
 }
 
